69551d04f98b83134bb68556e4d7a78e9f842d7f
twygw <stanley.dix@sky.uk>
2023-11-07 11:16:00 +0000

    init commit

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const app = require("./src/app")
+const app = require("./src/app");
 const db = require("./db/connection");
 const port = 3000;
 
-//TODO: Create your GET Request Route Below: 
-
+//TODO: Create your GET Request Route Below:
 
 app.listen(port, () => {
-    db.sync();
-    console.log(`Listening at http://localhost:${port}/restaurants`);
-})
+  db.sync();
+  console.log(`Listening at http://localhost:${port}/restaurants`);
+});
